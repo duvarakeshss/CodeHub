0c8532657cf6cb9b61a48fc6fd11c7f1d9c204c8
duvarakesh <duvarakesh05@gmail.com>
2024-10-20 21:47:10 +0530

    commit

--- Scl/wrk2/rk.py
+++ Scl/wrk2/rk.py
@@ -10,20 +10,17 @@ def f(fun,x0,y0):
     return fun.subs({x:x0 , y:y0}).evalf()
 
 def rk4(fun , c_x , c_y , h , x_predict):
-    x0 = c_x
-    y0 = c_y
+    x0, y0 = c_x , c_y
     
-    while x0 < x_predict:
-        
-        k1 = h * f(fun , x0 , y0)
-        k2 = h * f(fun , x0 + h/2 , y0 + k1/2)
-        k3 = h * f(fun , x0 + h/2 , y0+k2/2)
-        k4 = h * f(fun , x0 + h , y0 + k3)
-        
-        y0 = y0 + (1/6) * (k1 + 2*k2 + 2*k3 + k4)
-        
-        x0 += h
-    return y0
+    # Calculate the Runge-Kutta terms
+    k1 = h * f(function, x0, y0)
+    k2 = h * f(function, x0 + h / 2, y0 + k1 / 2)
+    k_prime = h * f(function, x0 + h, y0 + k1)
+    k3 = h * f(function, x0 + h, y0 + k_prime)
+
+    # Calculate the result using the third-order formula
+    result = y0 + (1 / 6) * (k1 + 4 * k2 + k3)
+    return result
 
 print(rk4(function, 0 , 1 , 0.2 , 0.2))
         
