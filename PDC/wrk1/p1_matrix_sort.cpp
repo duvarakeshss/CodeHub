e3e4d6af5b907704e828af09478b9208ce3437d2
Duvarakesh <131684523+duvarakeshss@users.noreply.github.com>
2025-12-08 16:11:27 +0000

    Refactor code structure for improved readability and maintainability

--- PDC/wrk1/p1_matrix_sort.cpp
+++ PDC/wrk1/p1_matrix_sort.cpp
@@ -5,12 +5,15 @@
 #include<algorithm>
 
 using namespace std;
+
+// Mutex for synchronizing access to the matrix
 mutex mtx;
 
 void RowSort(vector<vector<int>> & mat){
 
     lock_guard<mutex> lock(mtx);
 
+    // Sort even indexed rows in ascending order and odd indexed rows in descending order
     for(int i = 0 ; i < mat.size(); i++){
         if( i % 2 == 0){
             sort(mat[i].begin(), mat[i].end());
@@ -27,6 +30,7 @@ void columnSort(vector<vector<int>> & mat){
     int n = mat.size();
     int m = mat[0].size();
 
+    // Sort each column in ascending order
     for(int i = 0; i < m; i++){
 
         vector<int> col;
@@ -43,6 +47,8 @@ void columnSort(vector<vector<int>> & mat){
 }
 
 void printMatrix(const vector<vector<int>> &mat){
+    
+    // Print the matrix
     for(const auto &row : mat){
         for(const auto & elem : row){
             cout << elem << " ";
@@ -53,6 +59,7 @@ void printMatrix(const vector<vector<int>> &mat){
 
    int main(){
     
+    // initial matrix
     vector<vector<int>> matrix = {
         {45, 7, 9, 35},
         {12, 85, 16, 3},
@@ -65,17 +72,22 @@ void printMatrix(const vector<vector<int>> &mat){
     
    while(true){
 
+        // Store previous state of the matrix to check for stabilization
         vector<vector<int>> prev = matrix;
         
+        // Create and join threads for row and column sorting
         thread rowThread(RowSort, ref(matrix));
         rowThread.join();
         cout << "After Row Sort:" << endl;
         printMatrix(matrix);
 
+        // Create and join thread for column sorting
         thread colThread(columnSort, ref(matrix));
         colThread.join();
         cout << "After Column Sort:" << endl;
         printMatrix(matrix);
+
+        // Check for stabilization
         if(prev == matrix){
             cout << "Matrix stabilized. Exiting sorting loop." << endl;
             break;
